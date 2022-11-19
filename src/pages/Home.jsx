938fdab4225b6569d7df7d778ca1fa55c42dda2e
h4r0ldru1z <haroldand.ruiz@gmail.com>
2022-11-19 22:36:30 +0000

    desde cero

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../js/component/Navbar.jsx';
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home;
